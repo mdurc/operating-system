d6c742751e422e3c6ebd17953b4aca0ce07f9fca
mdurc <matthewdurcan@icloud.com>
2025-07-06 21:08:56 -0400

    bios print, text video print, keyhandler - updated file structure for readability

--- src/boot.asm
+++ src/boot.asm
@@ -9,6 +9,7 @@
   jmp start
 
   %include "print.inc"
+  %include "keyhandler.inc"
 
 start:
   ; 'cs' is the code segment where instructions live
@@ -26,42 +27,14 @@ start:
   mov ax, 0xb800  ; 16 * 0xb800 is the desired address
   mov es, ax
 
-  cli ; clear interrupts
-  mov bx, 0x09  ; keyboard hardware interrupt request (IRQ1)
-  shl bx, 2     ; multiply by 4 to get index in IVT (Interrupt Vector Table)
-  xor ax, ax
-  mov gs, ax    ; gs = 0, pointing to IVT base address 0x0000:0x0000
-  mov [gs:bx], WORD keyhandler  ; set low word (offset) of interrupt handler
-  mov [gs:bx+2], ds             ; set high word (segment) of interrupt handler
-  sti ; set interrupts
+  mov si, start_msg
+  call sprint
 
-  jmp $ ; jump to current address (infinite loop)
+  mov si, start_msg
+  call bios_print
 
-keyhandler:
-  in al, 0x60           ; read scan code from keyboard data port 60
-  mov bl, al            ; save in bl
-  mov BYTE[port60], al  ; store scan code in variable port60
-
-  in al, 0x61   ; read keyboard controller port
-  mov ah, al
-  or al, 0x80   ; set bit 7 to disable keyboard clock
-  out 0x61, al  ; send it back
-  xchg ah, al   ; swap ah and al
-  out 0x61, al  ; restore original port value
-
-  mov al, 0x20  ; send End of Interrupt (EOI) to PIC
-  out 0x20, al
-
-  and bl, 0x80  ; check if key released (bit 7 set)
-  jnz keyhandler_done      ; if key released, skip printing
-
-  mov ax, [port60]      ; load the scan code saved earlier
-  mov  WORD[reg16], ax  ; setup hex print
-  call printreg16
-keyhandler_done:
-  iret ; return from interrupt
-
-  port60 dw 0 ; for storing scan code from port 60 as keyboard data
+  call enable_keyhandler_int
+  jmp $
 
   ; ===========
 
